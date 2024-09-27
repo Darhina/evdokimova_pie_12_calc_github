ec34299c4b121130033e976ceadaa7364f83adf3
Darhina <darhina@bk.ru>
2024-09-27 22:07:32 +0700

    Добавлено описание программы в README и дополнена функция перевода в программе

--- Numeral System Converter.py
+++ Numeral System Converter.py
@@ -5,3 +5,14 @@ def convert_number(number, from_base, to_base):
     # Переводим исходное число в десятичную систему
     if from_base != 10:
         number = int(number, from_base)
+
+    # Переводим десятичное число в целевую систему счисления
+    if to_base == 2:
+        return bin(number).replace("0b", "")
+    elif to_base == 8:
+        return oct(number).replace("0o", "")
+    elif to_base == 16:
+        return hex(number).replace("0x", "").upper()
+    elif to_base == 10:
+        return str(number)
+
